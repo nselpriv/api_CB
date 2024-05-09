23271e8aab669d4a90616654051a23cbd7b9cbf9
nselpriv <nicolaiselo@gmail.com>
2024-05-09 14:04:19 +0200

    local config changes

--- tools/_playground.py
+++ tools/_playground.py
@@ -5,6 +5,16 @@ from zeeguu.api.app import create_app
 app = create_app()
 app.app_context().push()
 
-na = download_and_parse(
-    "https://www.dr.dk/stories/1288510966/allerede-inden-oscar-showets-start-lurer-en-ny-skandale-i-kulissen")
-print(na)
+#na = download_and_parse(
+#    "https://www.dr.dk/stories/1288510966/allerede-inden-oscar-showets-start-lurer-en-ny-skandale-i-kulissen")
+#print(na)
+
+
+from  zeeguu.core.content_recommender.elastic_recommender import content_recommendations
+
+
+print("yessir!")
+
+hits = content_recommendations(1911, 9)
+
+print (hits)
